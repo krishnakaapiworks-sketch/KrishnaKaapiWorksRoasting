7dcffab7f9bd2d1d8530d41854607c3eef5c30e4
krishnakaapiworks-sketch <krishnakaapiworks@gmail.com>
2025-11-02 11:32:15 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  assetPrefix: '/KrishnaKaapiWorksRoasting/',
+  basePath: '/KrishnaKaapiWorksRoasting',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
